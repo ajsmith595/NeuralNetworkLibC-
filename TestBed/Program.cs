ca3d18510cc865374373bff5bf295fa8e6e925c0
ajsmith595 <ajsmith595@hotmail.co.uk>
2018-03-10 18:45:44 +0000

    First commit - has matrix class in it

---
+++ TestBed/Program.cs
@@ -0,0 +1,34 @@
+﻿using NeuralNetworkLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace TestBed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix m1 = new Matrix(2, 3);
+            m1[0] = new List<float>() { 0, 1, 7 };
+            m1[1] = new List<float>() { 8, 9, 4 };
+
+            Console.WriteLine(m1);
+            Console.WriteLine(m1.Transposed());
+
+            Matrix m2 = new Matrix(3, 2);
+            m2[0] = new List<float>() { 9, 2 };
+            m2[1] = new List<float>() { 1, 3 };
+            m2[2] = new List<float>() { 0, 4 };
+            Console.WriteLine(m2);
+            m2.Transpose();
+            Console.WriteLine(m2);
+
+            m2.Transpose();
+
+
+            Matrix m = m1 * m2;
+            Console.WriteLine(m);
+            Console.Read();
+        }
+    }
+}
